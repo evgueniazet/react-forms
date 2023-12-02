f91106e9260a421f95f4f32d69c6d99289a94d23
Evguenia Zialenka <zelenkoea19@gmail.com>
2023-12-02 18:41:31 +0100

    refactor: move schema to separate file

--- src/components/HookForm/HookForm.tsx
+++ src/components/HookForm/HookForm.tsx
@@ -5,37 +5,7 @@ import * as yup from 'yup';
 import { IFormValues } from '../../interfaces/IFormValues';
 import { useDispatch } from 'react-redux';
 import { setFormData } from '../../store/reducers';
-
-const schema = yup.object({
-  name: yup
-    .string()
-    .matches(/^[A-Z][a-z]*$/, 'Name should start with an uppercase letter')
-    .required('Name is required'),
-  age: yup
-    .string()
-    .matches(/^\d+$/, 'Age should be a positive integer')
-    .required('Age is required'),
-  email: yup
-    .string()
-    .email('Must be a valid email')
-    .required('Email is required'),
-  password: yup
-    .string()
-    .matches(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-      'Password must contain at least 8 characters, including 1 uppercase letter, 1 lowercase letter, 1 number, and 1 special character'
-    )
-    .required('Password is required'),
-  passwordConfirm: yup
-    .string()
-    .oneOf([yup.ref('password')], 'Passwords must match')
-    .matches(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-      'Password must contain at least 8 characters, including 1 uppercase letter, 1 lowercase letter, 1 number, and 1 special character'
-    )
-    .required('Password confirmation is required'),
-  gender: yup.string().required('Gender is required'),
-});
+import { schema } from './schema';
 
 const HookForm: React.FC = () => {
   const dispatch = useDispatch();
